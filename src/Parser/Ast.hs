54cf528137f76b0ba08891b76ca7f25b11f62598
Jason KOENIG <jason.koenig@epitech.eu>
2025-11-22 22:43:15 +0100

    🏗️ split code into specialized sections

---
+++ src/Parser/Ast.hs
@@ -0,0 +1,32 @@
+{-
+-- EPITECH PROJECT, 2025
+-- Glados
+-- File description:
+-- LispToAst
+-}
+
+module Parser.Ast (sexprToAST) where
+
+import Lisp (SExpr(..))
+
+extractParam :: SExpr -> Maybe String
+extractParam (SSymbol s) = Just s
+extractParam _           = Nothing
+
+sexprToAST :: SExpr -> Maybe Ast
+sexprToAST (SInteger n) = Just (AInteger n)
+sexprToAST (SSymbol "#t") = Just (ABool True)
+sexprToAST (SSymbol "#f") = Just (ABool False)
+sexprToAST (SSymbol s) = Just (ASymbol s)
+sexprToAST (List (SSymbol "define" : SSymbol name : body : [])) = do
+    b <- sexprToAST body
+    return (Define name b)
+sexprToAST (List (SSymbol "define" : List (SSymbol name : params) : body : [])) = do
+    ps <- mapM extractParam params
+    b  <- sexprToAST body
+    return (DefineFun name ps b)
+sexprToAST (List (h:q)) = do
+    h2 <- sexprToAST h
+    q2 <- mapM sexprToAST q
+    return (Call h2 q2)
+sexprToAST _ = Nothing
