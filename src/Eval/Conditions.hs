7974297351336ad004f7e47606e16e0c9d010f76
Thomas Camuset <thomas.camuset@epitech.eu>
2025-11-24 17:33:54 +0100

    🚧 Error handling using 'Either' instead of 'Maybe'

--- src/Eval/Conditions.hs
+++ src/Eval/Conditions.hs
@@ -9,9 +9,12 @@ module Eval.Conditions (execCondition) where
 
 import Ast (Ast(..))
 
-execCondition :: Maybe Ast -> Maybe Ast -> Maybe Ast -> Maybe Ast
-execCondition (Just (ABool True)) th _ = th
-execCondition (Just (ABool False)) _ el = el
-execCondition (Just (AInteger 1)) th _ = th
-execCondition (Just (AInteger 0)) _ el = el
-execCondition _ _ _ = Nothing
+execCondition :: Maybe Ast -> Maybe Ast -> Maybe Ast -> Either String Ast
+execCondition (Just (ABool True)) (Just th) _ = Right th
+execCondition (Just (ABool False)) _ (Just el) = Right el
+execCondition (Just (AInteger 1)) (Just th) _ = Right th
+execCondition (Just (AInteger 0)) _ (Just el) = Right el
+execCondition Nothing _ _ = Left "*** ERROR: Condition is missing"
+execCondition (Just cond) _ _ = Left $ "*** ERROR: Invalid " ++
+    "condition: " ++ show cond
+execCondition _ _ _ = Left "*** ERROR: then or else branch is missing"
--- src/Eval/Functions.hs
+++ src/Eval/Functions.hs
@@ -14,10 +14,11 @@ import Utils.List (sameLength)
 type FuncTable = [(String, [String], Ast)]
 type Env = [(String, Ast)]
 
-registerFunction :: FuncTable -> String -> [String] -> Ast -> Maybe FuncTable
+registerFunction :: FuncTable -> String -> [String] -> Ast ->
+    Either String FuncTable
 registerFunction ftable name params body = case getFunction ftable name of
-    Just _  -> Nothing
-    Nothing -> Just ((name, params, body) : ftable)
+    Just _  -> Left $ "*** ERROR: Function already exists: " ++ name
+    Nothing -> Right $ (name, params, body) : ftable
 
 getFunction :: FuncTable -> String -> Maybe ([String], Ast)
 getFunction [] _ = Nothing
@@ -25,11 +26,17 @@ getFunction ((n, ps, b):xs) name
     | n == name = Just (ps, b)
     | otherwise = getFunction xs name
 
+execFunc :: (FuncTable -> Env -> Ast -> Maybe Ast) -> FuncTable -> Env ->
+    ([String], Ast, [Ast], String) -> Either String Ast
+execFunc evalFn ft env (p, body, args, name)
+    | not (sameLength p args) = Left $ "*** ERROR: Argument length " ++
+        "mismatch for function " ++ name
+    | otherwise = case evalFn ft (zip p args ++ env) body of
+        Just result -> Right result
+        Nothing -> Left $ "*** ERROR: Function evaluation failed" ++ name
+
 callFunction :: (FuncTable -> Env -> Ast -> Maybe Ast) ->
-    FuncTable -> Env -> String -> [Ast] -> Maybe Ast
+    FuncTable -> Env -> String -> [Ast] -> Either String Ast
 callFunction evalFn ftable env name args = case getFunction ftable name of
-    Nothing -> Nothing
-    Just (params, body)
-        | not (Utils.List.sameLength params args) -> Nothing
-        | otherwise -> let localEnv = zip params args ++ env in
-            evalFn ftable localEnv body
+    Nothing -> Left $ "*** ERROR: Unknown function: " ++ name
+    Just (p, body) -> execFunc evalFn ftable env (p, body, args, name)
