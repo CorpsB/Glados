d613d0e2793c704d5820ba1c6c1524f9f1d08aae
ncarabin <noe.carabin@epitech.eu>
2025-11-24 12:43:44 +0100

    👷 Add the coding-style checker script.

---
+++ tools/coding-style.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+##
+## EPITECH PROJECT, 2025
+## Glados
+## File description:
+## coding-style
+##
+
+check.sh "$(pwd)" "$(pwd)"
+
+error=false
+
+if test -s coding-style-reports.log; then
+    echo "Coding-style-report.log found ! Searching exceptions ..."
+
+    while IFS= read -r line; do
+
+        if [[ "$line" == *"main.hs"* ]]; then
+            continue
+        else 
+            echo "$line"
+            error=true
+        fi
+
+    done < "coding-style-reports.log"
+
+    if [[ "$error" == true ]]; then
+        exit 1
+    else
+        echo "All errors was ignored. Good work !"
+        exit 0
+    fi
+
+else
+    exit 0
+fi
