496320d916d05a182e37a00d9d3f31ccf9c37f34
Thomas CAMUSET <62751917+2k5Type0@users.noreply.github.com>
2025-11-24 17:40:15 +0100

    🔀 Merge 'feat/error-handling' into 'preprod' (#13)

--- src/Eval/Builtins.hs
+++ src/Eval/Builtins.hs
@@ -9,42 +9,50 @@ module Eval.Builtins (execBuiltin) where
 
 import Ast (Ast(..))
 
-builtinEq :: [Ast] -> Maybe Ast
-builtinEq [AInteger x, AInteger y] = Just $ ABool (x == y)
-builtinEq [ABool x, ABool y] = Just $ ABool (x == y)
-builtinEq _ = Nothing
+builtinEq :: [Ast] -> Either String Ast
+builtinEq [AInteger x, AInteger y] = Right $ ABool (x == y)
+builtinEq [ABool x, ABool y] = Right $ ABool (x == y)
+builtinEq args = Left $ "*** ERROR: 'eq?' expects two " ++
+    "integers or two booleans, got: " ++ show args
 
-builtinLowerThan :: [Ast] -> Maybe Ast
-builtinLowerThan [AInteger x, AInteger y] = Just $ ABool (x < y)
-builtinLowerThan _ = Nothing
+builtinLowerThan :: [Ast] -> Either String Ast
+builtinLowerThan [AInteger x, AInteger y] = Right $ ABool (x < y)
+builtinLowerThan args = Left $ "*** ERROR: '<' expects two " ++
+    "integers, got: " ++ show args
 
-builtinGreaterThan :: [Ast] -> Maybe Ast
-builtinGreaterThan [AInteger x, AInteger y] = Just $ ABool (x > y)
-builtinGreaterThan _ = Nothing
+builtinGreaterThan :: [Ast] -> Either String Ast
+builtinGreaterThan [AInteger x, AInteger y] = Right $ ABool (x > y)
+builtinGreaterThan args = Left $ "*** ERROR: '>' expects two " ++
+    "integers, got: " ++ show args
 
-builtinAddition :: [Ast] -> Maybe Ast
-builtinAddition [AInteger x, AInteger y] = Just $ AInteger (x + y)
-builtinAddition _ = Nothing
+builtinAddition :: [Ast] -> Either String Ast
+builtinAddition [AInteger x, AInteger y] = Right $ AInteger (x + y)
+builtinAddition args = Left $ "*** ERROR: '+' expects two " ++
+    "integers, got: " ++ show args
 
-builtinSubtraction :: [Ast] -> Maybe Ast
-builtinSubtraction [AInteger x, AInteger y] = Just $ AInteger (x - y)
-builtinSubtraction _ = Nothing
+builtinSubtraction :: [Ast] -> Either String Ast
+builtinSubtraction [AInteger x, AInteger y] = Right $ AInteger (x - y)
+builtinSubtraction args = Left $ "*** ERROR: '-' expects two " ++
+    "integers, got: " ++ show args
 
-builtinMultiplication :: [Ast] -> Maybe Ast
-builtinMultiplication [AInteger x, AInteger y] = Just $ AInteger (x * y)
-builtinMultiplication _ = Nothing
+builtinMultiplication :: [Ast] -> Either String Ast
+builtinMultiplication [AInteger x, AInteger y] = Right $ AInteger (x * y)
+builtinMultiplication args = Left $ "*** ERROR: '*' expects two " ++
+    "integers, got: " ++ show args
 
-builtinDivision :: [Ast] -> Maybe Ast
-builtinDivision [_, AInteger 0] = Nothing
-builtinDivision [AInteger x, AInteger y] = Just $ AInteger (div x y)
-builtinDivision _ = Nothing
+builtinDivision :: [Ast] -> Either String Ast
+builtinDivision [_, AInteger 0] = Left $ "*** ERROR: 'div' division by zero"
+builtinDivision [AInteger x, AInteger y] = Right $ AInteger (div x y)
+builtinDivision args = Left $ "*** ERROR: 'div' expects two " ++
+    "integers, got: " ++ show args
 
-builtinModulo :: [Ast] -> Maybe Ast
-builtinModulo [_, AInteger 0] = Nothing
-builtinModulo [AInteger x, AInteger y] = Just $ AInteger (mod x y)
-builtinModulo _ = Nothing
+builtinModulo :: [Ast] -> Either String Ast
+builtinModulo [_, AInteger 0] = Left $ "*** ERROR: 'mod' division by zero"
+builtinModulo [AInteger x, AInteger y] = Right $ AInteger (mod x y)
+builtinModulo args = Left $ "*** ERROR: 'mod' expects two " ++
+    "integers, got: " ++ show args
 
-execBuiltin :: String -> [Ast] -> Maybe Ast
+execBuiltin :: String -> [Ast] -> Either String Ast
 execBuiltin "eq?" as = builtinEq as
 execBuiltin "<" as = builtinLowerThan as
 execBuiltin ">" as = builtinGreaterThan as
@@ -53,4 +61,4 @@ execBuiltin "-" as = builtinSubtraction as
 execBuiltin "*" as = builtinMultiplication as
 execBuiltin "div" as = builtinDivision as
 execBuiltin "mod" as = builtinModulo as
-execBuiltin _ _ = Nothing
+execBuiltin op _ = Left $ "*** ERROR: Unknown builtin: " ++ op
