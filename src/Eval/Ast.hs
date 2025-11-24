0c1104e7c4bb5a9f995c1893cc6b22caab5c1882
Thomas CAMUSET <62751917+2k5Type0@users.noreply.github.com>
2025-11-24 17:40:15 +0100

    🔀 Merge 'feat/error-handling' into 'preprod' (#13)

--- src/Eval/Ast.hs
+++ src/Eval/Ast.hs
@@ -13,29 +13,35 @@ import Eval.Conditions (execCondition)
 import Eval.Functions (getFunction, FuncTable, Env)
 import Utils.List (sameLength)
 
-evalAST :: FuncTable -> Env -> Ast -> Maybe Ast
-evalAST _ _ (AInteger n) = Just (AInteger n)
-evalAST _ _ (ABool b) = Just (ABool b)
-evalAST _ env (ASymbol s) = lookupEnv env s
+execUserFunc :: FuncTable -> Env -> String -> [Ast] -> String -> Either String Ast
+execUserFunc ft env op args err = case getFunction ft op of
+    Just (p, b) | sameLength p args ->
+        evalASTEnv ft (zip p args ++ env) b
+    Just _ -> Left $ "*** ERROR: Argument length " ++
+        "missmatch for function " ++ op
+    Nothing -> Left err
+
+evalAST :: FuncTable -> Env -> Ast -> Either String Ast
+evalAST _ _ (AInteger n) = Right $ AInteger n
+evalAST _ _ (ABool b) = Right $ ABool b
+evalAST _ env (ASymbol s) = case lookupEnv env s of
+    Just v -> Right v
+    Nothing -> Left $ "*** ERROR: Undefined symbol: " ++ s
 evalAST ftable env (Define name body) = do
     b2 <- evalAST ftable env body
-    return (Define name b2)
-evalAST _ _ (DefineFun name params body) = Just (DefineFun name params body)
+    Right $ Define name b2
+evalAST _ _ (DefineFun name params body) = Right $ DefineFun name params body
 evalAST ftable env (Condition cond th el) = do
     c <- evalAST ftable env cond
     t <- evalAST ftable env th
     e <- evalAST ftable env el
-    execCondition (Just c) (Just t) (Just e)
+    execCondition (Just c) (Just t) (Just e) >>= Right
 evalAST ftable env (Call (ASymbol op) args) = do
     evalArgs <- traverse (evalASTEnv ftable env) args
     case execBuiltin op evalArgs of
-        Just r -> Just r
-        Nothing -> case getFunction ftable op of
-            Just (params, body) | Utils.List.sameLength params evalArgs ->
-                let localEnv = zip params evalArgs ++ env in
-                evalASTEnv ftable localEnv body
-            _ -> Nothing
-evalAST _ _ (Call _ _) = Nothing
+        Right r -> Right r
+        Left err -> execUserFunc ftable env op evalArgs err
+evalAST _ _ (Call _ _) = Left "*** ERROR: Invalid function call"
 
 lookupEnv :: Env -> String -> Maybe Ast
 lookupEnv [] _ = Nothing
@@ -43,10 +49,12 @@ lookupEnv ((k,v):xs) key
     | k == key  = Just v
     | otherwise = lookupEnv xs key
 
-evalASTEnv :: FuncTable -> Env -> Ast -> Maybe Ast
-evalASTEnv _ _ (AInteger n) = Just (AInteger n)
-evalASTEnv _ _ (ABool b) = Just (ABool b)
-evalASTEnv _ env (ASymbol s) = lookupEnv env s
+evalASTEnv :: FuncTable -> Env -> Ast -> Either String Ast
+evalASTEnv _ _ (AInteger n) = Right $ AInteger n
+evalASTEnv _ _ (ABool b) = Right $ ABool b
+evalASTEnv _ env (ASymbol s) = case lookupEnv env s of
+    Just v -> Right v
+    Nothing -> Left $ "*** ERROR: Undefined symbol: " ++ s
 evalASTEnv ftable env (Define name body) =
     evalAST ftable env (Define name body)
 evalASTEnv ftable env (DefineFun name params body) =
