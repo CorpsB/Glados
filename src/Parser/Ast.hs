5ac588a173ea46457dfd5fe71d941cc7d4f10796
Thomas CAMUSET <62751917+2k5Type0@users.noreply.github.com>
2025-11-24 16:13:30 +0100

    🔀 feat/conditional expressions (#12)
    * 🚧 Conditional expressions system
    
    * 🔥 Code beauty (some parentheses have been replaced by $)
    
    * 🚚 Move Ast.hs into original src/Eval directory (my mistake)
    
    * 🚧 Conditonal expressions 'if' to 'sexprToAST'
    
    * 🐛 Always Nothing when 'then' or 'else' is Nothing
    
    * ✨ Conditional expressions

--- src/Parser/Ast.hs
+++ src/Parser/Ast.hs
@@ -15,19 +15,24 @@ extractParam (SSymbol s) = Just s
 extractParam _           = Nothing
 
 sexprToAST :: SExpr -> Maybe Ast
-sexprToAST (SInteger n) = Just (AInteger n)
-sexprToAST (SSymbol "#t") = Just (ABool True)
-sexprToAST (SSymbol "#f") = Just (ABool False)
-sexprToAST (SSymbol s) = Just (ASymbol s)
+sexprToAST (SInteger n) = Just $ AInteger n
+sexprToAST (SSymbol "#t") = Just $ ABool True
+sexprToAST (SSymbol "#f") = Just $ ABool False
+sexprToAST (SSymbol s) = Just $ ASymbol s
 sexprToAST (List (SSymbol "define" : SSymbol name : body : [])) = do
     b <- sexprToAST body
-    return (Define name b)
+    return $ Define name b
 sexprToAST (List (SSymbol "define" : List (SSymbol name : params) : body : [])) = do
     ps <- mapM extractParam params
     b  <- sexprToAST body
-    return (DefineFun name ps b)
+    return $ DefineFun name ps b
+sexprToAST (List (SSymbol "if" : cond : th : el : [])) = do
+    c <- sexprToAST cond
+    t <- sexprToAST th
+    e <- sexprToAST el
+    return $ Condition c t e
 sexprToAST (List (h:q)) = do
     h2 <- sexprToAST h
     q2 <- mapM sexprToAST q
-    return (Call h2 q2)
+    return $ Call h2 q2
 sexprToAST _ = Nothing
