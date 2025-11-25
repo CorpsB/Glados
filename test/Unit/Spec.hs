ac3a6b72e5a32bbc3b73694f2dba0474432655ec
Turtelthib <146440577+Turtelthib@users.noreply.github.com>
2025-11-25 15:30:51 +0100

    🔀 Merge 'feat/ParserISLTest' into 'preprod' (#17)
    * ✅ add unit test for ParserISL
    
    * 🚚 mv ParserISLTest into test instead of src
    
    * 🔥 remove all coucou in test/
    
    * ✅ better coverage

--- test/Unit/Spec.hs
+++ test/Unit/Spec.hs
@@ -1,15 +1,13 @@
 module Main (main) where
 
 import Test.Hspec
-import qualified CoucouSpec
-import qualified Parser.CoucouSpec
-import qualified Eval.CoucouSpec
+import qualified Parser.ParserISLTest
+import qualified LispTest
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  CoucouSpec.spec
-  Parser.CoucouSpec.spec
-  Eval.CoucouSpec.spec
+  Parser.ParserISLTest.spec
+  LispTest.spec
