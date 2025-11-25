907e32f3e61e3eea7259d3e385efcf931213e7c1
Jason KOENIG <jason.koenig@epitech.eu>
2025-11-25 16:56:52 +0100

    ✅ eval.functions

--- test/Unit/Spec.hs
+++ test/Unit/Spec.hs
@@ -4,9 +4,12 @@ import Test.Hspec
 
 import qualified Eval.BuiltinsSpec
 import qualified Eval.ConditionsSpec
+import qualified Eval.FunctionsSpec
 import qualified AstSpec
 import qualified Parser.ParserISLTest
+import qualified Parser.AstSpec
 import qualified LispTest
+import qualified Utils.ListSpec
 
 main :: IO ()
 main = hspec spec
@@ -15,6 +18,9 @@ spec :: Spec
 spec = do
   Eval.BuiltinsSpec.spec
   Eval.ConditionsSpec.spec
+  Eval.FunctionsSpec.spec
   AstSpec.spec
   Parser.ParserISLTest.spec
+  Parser.AstSpec.spec
+  Utils.ListSpec.spec
   LispTest.spec
