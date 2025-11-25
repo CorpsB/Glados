b70e913fbfe2a8494d240770259818f95002ed98
Jason KOENIG <jason.koenig@epitech.eu>
2025-11-25 12:32:03 +0100

    🚧 basic lambda with closure (untested)

--- src/Eval/Ast.hs
+++ src/Eval/Ast.hs
@@ -7,11 +7,17 @@
 
 module Eval.Ast (evalAST, evalASTEnv) where
 
-import Ast (Ast(..))
+import Ast (Ast(..), Env)
 import Eval.Builtins (execBuiltin)
 import Eval.Conditions (execCondition)
-import Eval.Functions (getFunction, FuncTable, Env)
+import Eval.Functions (getFunction, FuncTable)
 import Utils.List (sameLength)
+
+applyClosure :: FuncTable -> Env -> [String] -> Ast -> [Ast] -> Either String Ast
+applyClosure ftable cEnv params body args
+    | sameLength params args =
+        evalASTEnv ftable (zip params args ++ cEnv) body
+    | otherwise = Left "*** ERROR: Argument length mismatch in lambda call"
 
 execUserFunc :: FuncTable -> Env -> String -> [Ast] -> String -> Either String Ast
 execUserFunc ft env op args err = case getFunction ft op of
@@ -27,6 +33,8 @@ evalAST _ _ (ABool b) = Right $ ABool b
 evalAST _ env (ASymbol s) = case lookupEnv env s of
     Just v -> Right v
     Nothing -> Left $ "*** ERROR: Undefined symbol: " ++ s
+evalAST _ env (Lambda params body) = Right $ Closure params body env
+evalAST _ _ (Closure p b e) = Right $ Closure p b e
 evalAST ftable env (Define name body) = do
     b2 <- evalAST ftable env body
     Right $ Define name b2
@@ -36,11 +44,19 @@ evalAST ftable env (Condition cond th el) = do
     t <- evalAST ftable env th
     e <- evalAST ftable env el
     execCondition (Just c) (Just t) (Just e) >>= Right
-evalAST ftable env (Call (ASymbol op) args) = do
+evalAST ftable env (Call func args) = do
     evalArgs <- traverse (evalASTEnv ftable env) args
-    case execBuiltin op evalArgs of
-        Right r -> Right r
-        Left err -> execUserFunc ftable env op evalArgs err
+    case func of
+        ASymbol op -> case execBuiltin op evalArgs of
+            Right r -> Right r
+            Left _ -> case lookupEnv env op of
+                Just (Closure p b cEnv) -> applyClosure ftable cEnv p b evalArgs
+                _ -> execUserFunc ftable env op evalArgs ("*** ERROR: Unknown function: " ++ op)
+        _ -> do
+            evaluatedFunc <- evalAST ftable env func
+            case evaluatedFunc of
+                Closure p b cEnv -> applyClosure ftable cEnv p b evalArgs
+                _ -> Left "*** ERROR: Attempt to call a non-function"
 evalAST _ _ (Call _ _) = Left "*** ERROR: Invalid function call"
 
 lookupEnv :: Env -> String -> Maybe Ast
@@ -55,8 +71,11 @@ evalASTEnv _ _ (ABool b) = Right $ ABool b
 evalASTEnv _ env (ASymbol s) = case lookupEnv env s of
     Just v -> Right v
     Nothing -> Left $ "*** ERROR: Undefined symbol: " ++ s
+evalASTEnv _ env (Lambda params body) = Right $ Closure params body env
+evalASTEnv _ _ (Closure p b e) = Right $ Closure p b e
 evalASTEnv ftable env (Define name body) =
     evalAST ftable env (Define name body)
 evalASTEnv ftable env (DefineFun name params body) =
     evalAST ftable env (DefineFun name params body)
 evalASTEnv ftable env (Call f args) = evalAST ftable env (Call f args)
+evalASTEnv ftable env (Condition c t e) = evalAST ftable env (Condition c t e)
