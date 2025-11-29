041aa4ad26e4439e6ed4b4d243e69ecce326bd11
Thomas Camuset <thomas.camuset@epitech.eu>
2025-11-29 17:45:15 +0100

    🐛 Coding style errors

--- src/Parser/Ast.hs
+++ src/Parser/Ast.hs
@@ -23,7 +23,8 @@ sexprToAST (List (SSymbol "lambda" : List params : body : [])) = do
     ps <- mapM extractParam params
     b  <- sexprToAST body
     Right $ Lambda ps b
-sexprToAST (List (SSymbol "lambda" : _)) = Left "*** ERROR: Invalid 'lambda' expression"
+sexprToAST (List (SSymbol "lambda" : _)) =
+    Left "*** ERROR: Invalid 'lambda' expression"
 sexprToAST (List (SSymbol "define" : SSymbol name : body : [])) = do
     b <- sexprToAST body
     Right $ Define name b
@@ -31,7 +32,8 @@ sexprToAST (List (SSymbol "define" : List (SSymbol name : params) : body : [])) 
     ps <- mapM extractParam params
     b  <- sexprToAST body
     Right $ DefineFun name ps b
-sexprToAST (List (SSymbol "define" : _)) = Left "*** ERROR: Invalid 'define' expression"
+sexprToAST (List (SSymbol "define" : _)) =
+    Left "*** ERROR: Invalid 'define' expression"
 sexprToAST (List (SSymbol "if" : cond : th : el : [])) = do
     c <- sexprToAST cond
     t <- sexprToAST th
