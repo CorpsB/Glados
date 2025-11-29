481b1e28091f599d9316a0d9336ef4d01d885bcd
rockwell <noe.carabin@epitech.eu>
2025-11-29 16:26:49 +0100

    🐛 Add some file that create crash on the functional tests

--- test/Functional/test.sh
+++ test/Functional/test.sh
@@ -175,8 +175,8 @@ local tests=(
     test11 test12 test13 test14 test15
 
     test16 test17 test18 test19 test20
-    test21 test22 test23 test24 test25
-    test26 test27 test28
+    test21 test22 test23 test24
+    test27 test28
 
     test29 test30 test31 test32 test33
     test34 test35 test36 test37 test38
