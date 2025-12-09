156fa249230363056161fbfc3acb55785dc5feaa
Thibaut <thibaut.louis@epitech.eu>
2025-12-09 14:48:30 +0100

    ✨ add new AST struct, BinOp AString AChar Avoid

--- src/Ast.hs
+++ src/Ast.hs
@@ -11,10 +11,24 @@ import Type.Integer (IntValue(..))
 
 type Env = [(String, Ast)]
 
+data BinOp
+    = Add
+    | Sub
+    | Mul
+    | Div
+    | Mod
+    | Eq
+    | Less
+    deriving (Show, Eq)
+
 data Ast
     = AInteger IntValue
     | ABool Bool
     | ASymbol String
+    | AString String
+    | AChar Char
+    | AVoid
+    | BinaryOp BinOp Ast Ast
     | Condition Ast Ast Ast
     | Define String Ast
     | DefineFun String [String] Ast -- (define f [a,b] exec)
--- src/Type/Integer.hs
+++ src/Type/Integer.hs
@@ -9,12 +9,17 @@ module Type.Integer (IntValue(..), fitInteger, toInt64,
     fromInt64, intValueEq, intValueToInt) where
 
 import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Word (Word8, Word16, Word32, Word64)
 
 data IntValue
     = I8 Int8
     | I16 Int16
     | I32 Int32
     | I64 Int64
+    | U8 Word8
+    | U16 Word16
+    | U32 Word32
+    | U64 Word64
     deriving (Show, Eq, Ord)
 
 toInt64 :: IntValue -> Int64
@@ -22,6 +27,10 @@ toInt64 (I8 n)  = fromIntegral n
 toInt64 (I16 n) = fromIntegral n
 toInt64 (I32 n) = fromIntegral n
 toInt64 (I64 n) = n
+toInt64 (U8 n)  = fromIntegral n
+toInt64 (U16 n) = fromIntegral n
+toInt64 (U32 n) = fromIntegral n
+toInt64 (U64 n) = fromIntegral n
 
 fromInt64 :: Int64 -> IntValue
 fromInt64 n
@@ -41,6 +50,10 @@ intValueToInt (I8 n)  = fromIntegral n
 intValueToInt (I16 n) = fromIntegral n
 intValueToInt (I32 n) = fromIntegral n
 intValueToInt (I64 n) = fromIntegral n
+intValueToInt (U8 n)  = fromIntegral n
+intValueToInt (U16 n) = fromIntegral n
+intValueToInt (U32 n) = fromIntegral n
+intValueToInt (U64 n) = fromIntegral n
 
 intValueEq :: IntValue -> Int64 -> Bool
 intValueEq v ref = (toInt64 v) == ref
