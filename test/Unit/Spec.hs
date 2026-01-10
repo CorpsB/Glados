430875abe17f4f61b99493957ce6d1e55c46598d
Pierre-Louis SCHORSCH <pierrelouisschorsch@gmail.com>
2026-01-10 22:39:56 +0100

    ✅ Unit Test VM

--- test/Unit/Spec.hs
+++ test/Unit/Spec.hs
@@ -28,6 +28,19 @@ import qualified Z_old.AstSpec
 import qualified Z_old.Type.IntegerSpec
 import qualified Z_old.Utils.ListSpec
 import qualified Common.Type.IntegerSpec
+import qualified VM.Bytecode.ReaderSpec
+import qualified VM.Bytecode.RunnerSpec
+import qualified VM.Instruction.ArithmeticSpec
+import qualified VM.Instruction.FunctionSpec
+import qualified VM.Instruction.IndexSpec
+import qualified VM.Instruction.LogicSpec
+import qualified VM.Instruction.StackSpec
+import qualified VM.Instruction.SystemSpec
+import qualified VM.Instruction.VariableSpec
+import qualified VM.CallSnapshotSpec
+import qualified VM.VMStateSpec
+import qualified VM.VMStackSpec
+import qualified VM.VMValueSpec
 
 main :: IO ()
 main = hspec spec
@@ -58,3 +71,16 @@ spec = do
     Compiler.ASM.AstToAsmSpec.spec
     Compiler.ASM.CompilerSpec.spec
     Common.Type.IntegerSpec.spec
+    VM.Bytecode.ReaderSpec.spec
+    VM.Bytecode.RunnerSpec.spec
+    VM.Instruction.ArithmeticSpec.spec
+    VM.Instruction.FunctionSpec.spec
+    VM.Instruction.IndexSpec.spec
+    VM.Instruction.LogicSpec.spec
+    VM.Instruction.StackSpec.spec
+    VM.Instruction.SystemSpec.spec
+    VM.Instruction.VariableSpec.spec
+    VM.CallSnapshotSpec.spec
+    VM.VMStateSpec.spec
+    VM.VMStackSpec.spec
+    VM.VMValueSpec.spec
