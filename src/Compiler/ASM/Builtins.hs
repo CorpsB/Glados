50645af37350e7ce37ad9b6109566bb3d6512138
Jason KOENIG <jason.koenig@epitech.eu>
2026-01-17 18:48:05 +0100

    ✨ nth_update builtin handling

--- src/Compiler/ASM/Builtins.hs
+++ src/Compiler/ASM/Builtins.hs
@@ -29,7 +29,8 @@ builtinArithList =
 builtinStackList :: [(Text, Instruction)]
 builtinStackList =
     [ (pack "cons", Cons), (pack "head", Head),
-    (pack "tail", Tail), (pack "nth", Nth) ]
+    (pack "tail", Tail), (pack "nth", Nth),
+    (pack "nth_update", NthUpdate) ]
 
 builtinLogicList :: [(Text, Instruction)]
 builtinLogicList =
