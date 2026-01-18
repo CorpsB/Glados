b42085e4cae90868015675f7bc54003dd2df5400
Thibaut <thibaut.louis@epitech.eu>
2026-01-18 14:06:31 +0100

    ✨ add new builtins for files lib

--- src/AST/Semantics/Check.hs
+++ src/AST/Semantics/Check.hs
@@ -42,14 +42,6 @@ insertVar env name t =
     let newVars = Map.insert name t (envVars env)
     in Right $ env { envVars = newVars }
 
--- Helper to check a sequence
-checkBlock :: CheckEnv -> [Ast] -> Either String Type
-checkBlock _ [] = Right TyVoid
-checkBlock env [x] = checkExpr env x
-checkBlock env (x:xs) = do
-    _ <- checkExpr env x
-    checkBlock env xs
-
 -- | Check Expression: Verifies an expression and returns its Semantic Type.
 --
 -- Does not modify the AST, only extracts the type.
@@ -164,6 +156,7 @@ checkStmt env (AImport _) = Right (AVoid, env)
 checkStmt env (AExprStmt stmt) = do
     (newStmt, newEnv) <- checkStmt env stmt
     return (AExprStmt newStmt, newEnv)
+checkStmt env ABreak = Right (ABreak, env)
 checkStmt env ast = do
     _ <- checkExpr env ast
     return (ast, env)
