dba768f3edd5b1263ca8713599a94c069771b7ca
Thomas CAMUSET <62751917+2k5Type0@users.noreply.github.com>
2026-01-18 23:25:22 +0100

    :twisted_rightwards_arrows: feat/disasm (#66)
    * ✨ Compiler's disassembler
    
    * 🔥 Remove useless comments
    
    * 🐛 Fix some coding style errors
    
    * ✨ crocus in .npy
    
    * 🔥 remove test.npy at the root

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,9 @@ import Data.Foldable (toList)
 import qualified Data.Sequence as Seq
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
+import Numeric (showHex)
+import Data.Char (toUpper)
+import Control.Monad (forM_)
 
 import Parser.Statement (parseALL)
 import Parser.ImportSystem (resolveImports)
@@ -28,16 +31,15 @@ import Compiler.Instruction (Instruction(Halt))
 import Common.Utils.Bytecode.File (getFileContent)
 import AST.Semantics.Check (checkAst)
 import AST.Ast (Ast)
+import Compiler.Bytecode.DisasmFinal (disasmFinal)
 
 -- | Print usage message on stdout.
---
 usage :: IO ()
-usage = putStrLn ("Usage: ./glados <input_file> <output_file>"
-    ++ "  input_file:  Path to the source code."
-    ++ "  output_file: Path to the generated binary (.gla).")
+usage = putStrLn ("Usage:\n"
+    ++ "  ./glados <input_file> <output_file>   (Compile source to binary)\n"
+    ++ "  ./glados --disasm <binary_file>       (Disassemble binary file)")
 
 -- | Print the given error message then exit 84.
---
 die :: String -> IO a
 die msg = hPutStrLn stderr ("\ESC[31mError:\ESC[0m " ++ msg) >>
     exitWith (ExitFailure 84)
@@ -46,7 +48,6 @@ parseSource :: T.Text -> IO [Ast]
 parseSource src = case parseALL src of
     Left err -> die $ "Parsing failed:\n" ++ show err
     Right asts -> return asts
-
 
 handleImports :: [Ast] -> IO [Ast]
 handleImports asts = do
@@ -91,9 +92,34 @@ runCompiler inputPath outputPath = do
     bytecode <- assembleCode (extractInstructions state)
     writeBinary outputPath bytecode
 
+showHex4 :: Int -> String
+showHex4 x = 
+    let h = showHex x ""
+        padded = replicate (4 - length h) '0' ++ h
+    in map toUpper padded
+
+runDisasm :: String -> IO ()
+runDisasm path = do
+    content <- BS.readFile path
+
+    if BS.length content < 10
+        then die "File is too small to be a valid Glados binary."
+        else return ()
+
+    let bytecode = BS.drop 10 content
+
+    case disasmFinal bytecode of
+        Left err -> die $ T.unpack err
+        Right instrs -> do
+            putStrLn "Offset | Instruction"
+            putStrLn "-------|-------------------------------------------"
+            forM_ instrs $ \(off, str) -> do
+                putStrLn $ showHex4 off ++ "   | " ++ str
+
 main :: IO ()
 main = do
     args <- getArgs
     case args of
+        ["--disasm", file] -> runDisasm file
         [input, output] -> runCompiler input output
         _ -> usage >> exitWith (ExitFailure 84)
